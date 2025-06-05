f2c205d1bf786e467159967b2989ec7a9adafcce
Wichtra <willy.chairullah@gmail.com>
2025-06-05 09:11:39 +0700

    refactor: update DashboardLayout styling and remove unused Versions component

--- src/renderer/src/app/layout/DashboardLayout.jsx
+++ src/renderer/src/app/layout/DashboardLayout.jsx
@@ -6,7 +6,7 @@ import Breadcrumb from "../../shared/ui/BreadCrumb";
 const DashboardLayout = () => {
   return (
     <>
-      <div className="container max-w-[1200px] mx-auto flex flex-col sm:flex-row justify-between gap-x-8 px-4">
+      <div className="w-full mx-auto flex flex-col sm:flex-row justify-between gap-x-8">
         <div className="w-[300px]">
           <Sidebar />
         </div>
